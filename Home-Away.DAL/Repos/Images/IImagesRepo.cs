306825bd9fa5eed65beaddcb14610c94dafdbb7c
Omar8895 <omarelarabe9@gmail.com>
2023-05-31 23:43:50 +0300

    last-commit

--- Home-Away.DAL/Repos/Images/IImagesRepo.cs
+++ Home-Away.DAL/Repos/Images/IImagesRepo.cs
@@ -3,4 +3,11 @@
 namespace Home_Away.DAL;
     public interface IImagesRepo
     {
+        public IEnumerable<Images> GetAllImages();
+        public Images? GetImagesById(int id);
+        public IEnumerable<Images> GetImagesByProperty(int Prop_id);
+        public void AddImage(Images image);
+        public void UpdateImage(Images image);
+        public void DeleteImage(Images image);
+        public int SaveChanges();
     }
--- Home-Away.DAL/Repos/Property/PropertyRepo.cs
+++ Home-Away.DAL/Repos/Property/PropertyRepo.cs
@@ -1,5 +1,69 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
+
 namespace Home_Away.DAL;
-    public class PropertyRepo
+   public class PropertyRepo : IPropertyRepo
+   {
+     
+    private readonly UserContext _userContext;
+
+    public PropertyRepo(UserContext userContext)
     {
+        _userContext = userContext;
     }
+
+    public IEnumerable<Property> GetAllProperties()
+    {
+        return _userContext.Set<Property>().AsNoTracking();
+    }
+
+    public Property? GetPropertyById (int id)
+    {
+        return _userContext.Set<Property>().Find(id);
+    }
+
+    public IEnumerable<Property>? GetPropertyByOwner(string owner_id)
+    {
+        return _userContext.Set<Property>().Where(o => o.OwnerId == owner_id);
+    }
+    public IEnumerable<Property>? GetPropertyByAdmin(string admin_id)
+    {
+        return _userContext.Set<Property>().Where(o => o.AdminId == admin_id);
+    }
+
+    public int GetPropertyByImage(int image_id)
+    {
+        var image = _userContext.Set<Images>().FirstOrDefault(p => p.Id == image_id);
+        return image.PropertyId;
+    }
+    public IEnumerable<Property>? GetPropertyByState(string state)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Property>? FilterProperty(string? title, string? address, string? Type, string? region, string? area, string? category, decimal? price_per_night, int? capacity, int? no_of_rooms, int? no_of_bathrooms, int? no_of_floors, decimal? avg_rating)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddProperty(Property property)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteProperty(Property property)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateProperty(Property property)
+    {
+        
+    }
+
+    public int SaveChanges()
+    {
+        return _userContext.SaveChanges();
+    }
+
+}
