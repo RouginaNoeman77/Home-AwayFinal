d40c11a97bdef2ed9e7c17ae2d5ff17e90ced174
RouginaNoeman77 <rougina.noeman@gmail.com>
2023-05-31 23:26:34 +0300

    Merging Property Class

--- Home-Away.DAL/Repos/Property/IPropertyRepo.cs
+++ Home-Away.DAL/Repos/Property/IPropertyRepo.cs
@@ -1,5 +1,16 @@
-﻿
-namespace Home_Away.DAL;
-    public interface IPropertyRepo
-    {
-    }
+﻿namespace Home_Away.DAL;
+public interface IPropertyRepo
+{
+    IEnumerable<Property> GetAllProperties();
+    Property? GetPropertyById(int id);
+    Property? GetPropertyByOwner(string owner_id);
+    Property? GetPropertyByState(string state); 
+    Property? GetPropertyByAdmin(string admin_id);
+    Property? GetPropertyByImage(int image_id);
+    Property FilterProperty(string? title, string? address, string? Type, string? region, string? area, string? category, decimal? price_per_night, int? capacity, int? no_of_rooms, int? no_of_bathrooms, int? no_of_floors , decimal? avg_rating);
+    void AddProperty(Property property);
+    void UpdateProperty(Property property);
+    void DeleteProperty(Property property);
+    int SaveChanges();
+
+}
--- Home-Away.DAL/Repos/Property/PropertyRepo.cs
+++ Home-Away.DAL/Repos/Property/PropertyRepo.cs
@@ -1,5 +1,66 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+
 namespace Home_Away.DAL;
-    public class PropertyRepo
+public class PropertyRepo : IPropertyRepo
+{
+    private readonly UserContext _userContext;
+    public PropertyRepo(UserContext userContext)
     {
+        _userContext = userContext;
     }
+
+    public IEnumerable<Property> GetAllProperties()
+    {
+        return _userContext.Set<Property>().AsNoTracking();
+    }
+
+    public Property? GetPropertyById(int id)
+    {
+        return _userContext.Set<Property>().Find(id);
+    }
+
+    public Property? GetPropertyByOwner(string owner_id)
+    {
+        return _userContext.Set<Property>().FirstOrDefault(o => o.OwnerId == owner_id);
+    }
+    public Property? GetPropertyByAdmin(string admin_id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Property? GetPropertyByImage(int image_id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Property? GetPropertyByState(string state)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Property FilterProperty(string? title, string? address, string? Type, string? region, string? area, string? category, decimal? price_per_night, int? capacity, int? no_of_rooms, int? no_of_bathrooms, int? no_of_floors, decimal? avg_rating)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddProperty(Property property)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteProperty(Property property)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateProperty(Property property)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int SaveChanges()
+    {
+        return _userContext.SaveChanges();
+    }
+
+}
