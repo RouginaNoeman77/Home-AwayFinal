8463ad5322f767bdb591e957e513aa32ad979b30
Hatem <M.Hatem@gmail.com>
2023-06-01 09:19:57 +0300

    first review branch

--- Home-Away.DAL/Repos/Reviews/IReviewsRepo.cs
+++ Home-Away.DAL/Repos/Reviews/IReviewsRepo.cs
@@ -2,4 +2,17 @@
 namespace Home_Away.DAL;
     public interface IReviewsRepo
     {
-    }
+        IEnumerable<Reviews> GetReviews();    
+        Reviews? GetReviewbyId(int id);
+
+        IEnumerable<Reviews> GetReviewsByDate(DateTime datefrom);
+
+        IEnumerable<Reviews> GetReviewsByStatus(string status);
+
+        int AddReview(Reviews review);
+        void UpdateReview(Reviews review);
+
+        int DeleteReview(Reviews review);
+
+        int SaveChanges();
+}
--- Home-Away.DAL/Repos/Reviews/ReviewsRepo.cs
+++ Home-Away.DAL/Repos/Reviews/ReviewsRepo.cs
@@ -1,5 +1,54 @@
 ﻿
 namespace Home_Away.DAL;
-    public class ReviewsRepo
+public class ReviewsRepo : IReviewsRepo
+{
+   private readonly UserContext _context;
+
+    public ReviewsRepo(UserContext context)
     {
+        _context = context;
     }
+
+    public IEnumerable<Reviews> GetReviews()
+    {
+        return _context.Set<Reviews>();
+    }
+
+    public Reviews? GetReviewbyId(int id)
+    {
+        return _context.Set<Reviews>().Find(id);
+    }
+
+    public IEnumerable<Reviews> GetReviewsByDate(DateTime datefrom)
+    {
+        return _context.Set<Reviews>().Where(a => a.ReviewDate > datefrom);
+    }
+
+    public IEnumerable<Reviews> GetReviewsByStatus(string status)
+    {
+        return _context.Set<Reviews>().Where(a=>a.State == status);
+    }
+    public int AddReview(Reviews review)
+    {
+        _context.Add(review);
+        SaveChanges();
+        return 1;
+    }
+
+    public int DeleteReview(Reviews review)
+    {
+        _context.Set<Reviews>().Remove(review);
+        return 1;
+    }
+
+    public int SaveChanges()
+    {
+        return _context.SaveChanges();
+
+    }
+
+    public void UpdateReview(Reviews review)
+    {
+        //works by tracking so ne need to implement code
+    }
+}
