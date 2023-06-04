96925af216338dc3477cbb369502cccd9b6a42ce
Eng.Zeyad <Eng.Zeyad@DESKTOP-3OBJOOE>
2023-06-04 19:35:28 +0300

    changes

---
+++ Home-Away.BL/Dtos/User_Answer_Dto/User_Answer_AddDto.cs
@@ -0,0 +1,16 @@
+﻿using Home_Away.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Away.BL.Dtos.User_Answer_Dto
+{
+    public class User_Answer_AddDto
+    {
+        public string UserAnswers { get; set; } = string.Empty;
+        public string UserId { get; set; }
+        public int QuestionsId { get; set; }
+    }
+}
